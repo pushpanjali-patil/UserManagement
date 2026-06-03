b5645ceeb784382ac46d782a56db3f1c36af9df0
pushpanjali-patil <56927198+pushpanjali-patil@users.noreply.github.com>
2026-06-03 10:14:51 +0530

    Update Student.cs

--- Student.cs
+++ Student.cs
@@ -1,492 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
+// See https://aka.ms/new-console-template for more information
 
-public class Stock
+using System.Security.Cryptography.X509Certificates;
+
+Dictionary<string,List<string>>   StudentCourse = new Dictionary<string,List<string>>();
+
+(string RollNo, string Course)[] data =
 {
-    // Data about a particular stock
-    public string Symbol;
-    public string Name;
+    ("A","Math"),
+    ("A","Science"),
+    ("B","Math"),
+    ("B","Social Science")
+};
 
-    public Stock(string symbol, string name)
+foreach(var item in data)
+{
+    string RollNumber = item.RollNo;
+    string Course = item.Course;
+
+    if(!StudentCourse.ContainsKey(RollNumber))
     {
-        Symbol = symbol;
-        Name = name;
+        StudentCourse[RollNumber] = [];
     }
+    StudentCourse[RollNumber].Add(Course);
+
+}
+var RollNumbers= StudentCourse.Keys.ToList();
 
 
-    public override bool Equals(object other)
+
+Dictionary<string,List< string>> dict = new Dictionary<string,List< string>>();
+
+for(int i=0;i<RollNumbers.Count;i++)
+{
+    for(int j=i+1;j< RollNumbers.Count;j++)
     {
-        if (this == other)
-            return true;
+        string first = RollNumbers[i];
+        string second = RollNumbers[j];
 
-        if (other == null ||
-            GetType() != other.GetType())
-            return false;
+        var Students = first + "," + second;
+        var FirstStudentCourse = StudentCourse[first].ToList();
 
-        Stock stock = (Stock)other;
+        var SecondStudentCourse = StudentCourse[second].ToList();
 
-        return Symbol == stock.Symbol
-               && Name == stock.Name;
-    }
+        var commonCourse = FirstStudentCourse.Intersect(SecondStudentCourse).ToList();
 
-    public override int GetHashCode()
-    {
-        return HashCode.Combine(
-            Symbol,
-            Name);
+        dict[Students]= commonCourse;
+
     }
 }
 
-public class Result
+
+foreach (var item in dict)
 {
-    public int Difference { get; set; }
-
-    public string StartDate { get; set; }
-    public string EndDate { get; set; }
-
-    
-}
-
-    public class PriceRecord
-{
-    // Single price record
-    public Stock Stock;
-    public int Price;
-    public string Date;
-
-    public PriceRecord(
-        Stock stock,
-        int price,
-        string date)
-    {
-        Stock = stock;
-        Price = price;
-        Date = date;
-    }
-}
-
-public class StockCollection
-{
-    public List<PriceRecord>
-        PriceRecords =
-        new List<PriceRecord>();
-
-    public Stock Stock;
-
-    public StockCollection(
-        Stock stock)
-    {
-        Stock = stock;
-    }
-
-    public int GetNumPriceRecords()
-    {
-        Console.WriteLine(
-            "PriceRecords.Count >> "
-            + PriceRecords.Count);
-
-        return PriceRecords.Count;
-    }
-
-    public void AddPriceRecord(
-        PriceRecord priceRecord)
-    {
-        if (!priceRecord.Stock
-            .Equals(this.Stock))
-        {
-            throw new Exception(
-              "PriceRecord Stock mismatch");
-        }
-
-        PriceRecords.Add(
-            priceRecord);
-    }
-
-    public int GetMaxPrice()
-    {
-        if (!PriceRecords.Any())
-        {
-            return -1;
-        }
-
-        return PriceRecords
-            .Max(record =>
-                 record.Price);
-    }
-
-    public int GetMinPrice()
-    {
-        if (!PriceRecords.Any())
-        {
-            return -1;
-        }
-
-        return PriceRecords
-            .Min(record =>
-                 record.Price);
-    }
-
-    public double GetAvgPrice()
-    {
-        if (!PriceRecords.Any())
-        {
-            return -1;
-        }
-
-        double total =
-            PriceRecords
-            .Sum(record =>
-                 record.Price);
-
-        return total /
-               PriceRecords.Count;
-    }
-
-    public Result GetBiggestChange()
-    {
-
-        var result = PriceRecords.OrderBy(x => x.Date).ToList();
-        
-
-        if (result.Count > 1)
-        {
-            Result obj = new Result();
-            int biggestChage = int.MinValue;
-
-
-            for (var i = 1; i < result.Count; i++)
-            {
-                var current = result[i].Price;
-
-                var previous = result[i - 1].Price;
-
-                int difference =Math.Abs(current - previous);
-
-                if (difference > biggestChage)
-                {
-                    biggestChage = difference;
-
-                    obj.Difference = current - previous;
-                    obj.StartDate = result[i - 1].Date;
-                    obj.EndDate = result[i].Date;
-                }
-
-            }
-            return obj;
-        }
-        else
-        {
-            return null;
-        }
-        
-    }
-}
+    Console.WriteLine(item.Key+"->"+ string.Join(",",item.Value));
     
 
-public class Program
-{
-    static void Main()
-    {
-        TestPriceRecord();
-
-        TestStockCollection();
-        TestGetBiggestChange();
-    }
-
-    static void TestPriceRecord()
-    {
-        Console.WriteLine(
-        "Running TestPriceRecord");
-
-        Stock testStock =
-            new Stock(
-                "AAPL",
-                "Apple Inc.");
-
-        PriceRecord testRecord =
-            new PriceRecord(
-                testStock,
-                100,
-                "2023-07-01");
-
-        Debug.Assert(
-            testRecord.Stock
-            .Equals(testStock));
-
-        Debug.Assert(
-            testRecord.Price
-            == 100);
-
-        Debug.Assert(
-            testRecord.Date
-            == "2023-07-01");
-    }
-
-
-    static StockCollection
-    MakeStockCollection(
-    Stock stock,
-    object[][] priceData)
-    {
-        StockCollection
-            stockCollection =
-            new StockCollection(
-                stock);
-
-        foreach (var data
-                in priceData)
-        {
-            PriceRecord
-                priceRecord =
-                new PriceRecord(
-                    stock,
-                    (int)data[0],
-                    (string)data[1]);
-
-            stockCollection
-                .AddPriceRecord(
-                    priceRecord);
-        }
-
-        return stockCollection;
-    }
-
-
-    static void TestStockCollection()
-    {
-        Console.WriteLine(
-        "Running TestStockCollection");
-
-        Stock testStock =
-            new Stock(
-                "AAPL",
-                "Apple Inc.");
-
-        StockCollection
-        stockCollection =
-        new StockCollection(
-            testStock);
-
-        Debug.Assert(
-            stockCollection
-            .GetNumPriceRecords()
-            == 0);
-
-        Debug.Assert(
-            stockCollection
-            .GetMaxPrice()
-            == -1);
-
-        Debug.Assert(
-            stockCollection
-            .GetMinPrice()
-            == -1);
-
-        Debug.Assert(
-            stockCollection
-            .GetAvgPrice()
-            == -1);
-
-
-        object[][] priceData =
-        {
-            new object[]
-            {
-                110,
-                "2023-06-29"
-            },
-
-            new object[]
-            {
-                112,
-                "2023-07-01"
-            },
-
-            new object[]
-            {
-                90,
-                "2023-06-28"
-            },
-
-            new object[]
-            {
-                105,
-                "2023-07-06"
-            }
-        };
-
-        testStock =
-            new Stock(
-                "AAPL",
-                "Apple Inc.");
-
-        stockCollection =
-            MakeStockCollection(
-                testStock,
-                priceData);
-
-        Debug.Assert(
-            priceData.Length
-            ==
-            stockCollection
-            .GetNumPriceRecords());
-
-        Debug.Assert(
-            stockCollection
-            .GetMaxPrice()
-            == 112);
-
-        Debug.Assert(
-            stockCollection
-            .GetMinPrice()
-            == 90);
-
-        Debug.Assert(
-            Math.Abs(
-            stockCollection
-            .GetAvgPrice()
-            - 104.25)
-            < 0.1);
-
-        
-    }
-
-    public static void TestGetBiggestChange()
-    {
-        Console.WriteLine(
-            "Running TestGetBiggestChange");
-
-        // Test the GetBiggestChange method
-
-        Console.WriteLine(
-            "Running TestGetBiggestChange");
-
-        
-
-        Stock testStock =
-            new Stock(
-                "AAPL",
-                "Apple Inc.");
-
-        StockCollection stockCollection =
-            new StockCollection(
-                testStock);
-
-
-
-        Debug.Assert(
-            stockCollection
-            .GetBiggestChange()
-            == null);
-
-        //    Debug.Assert(
-        //        stockCollection
-        //        .GetBiggestChange()
-        //        == null);
-
-
-        //    object[][] priceData =
-        //    {
-        //    new object[]
-        //    {
-        //        110,
-        //        "2023-06-29"
-        //    },
-
-        //    new object[]
-        //    {
-        //        112,
-        //        "2023-07-01"
-        //    },
-
-        //    new object[]
-        //    {
-        //        90,
-        //        "2023-06-25"
-        //    },
-
-        //    new object[]
-        //    {
-        //        105,
-        //        "2023-07-06"
-        //    }
-        //};
-
-        //    stockCollection =
-        //        MakeStockCollection(
-        //            testStock,
-        //            priceData);
-
-
-        //    Result result =
-        //        stockCollection
-        //        .GetBiggestChange();
-
-        //    Debug.Assert(
-        //        result.Difference == 20);
-
-        //    Debug.Assert(
-        //        result.StartDate
-        //        == "2023-06-25");
-
-        //    Debug.Assert(
-        //        result.EndDate
-        //        == "2023-06-29");
-
-
-
-        object[][] priceData2 =
-        {
-        new object[]
-        {
-            200,
-            "2000-01-04"
-        },
-
-        new object[]
-        {
-            210,
-            "1999-12-30"
-        },
-
-        new object[]
-        {
-            190,
-            "2000-01-03"
-        },
-
-        new object[]
-        {
-            180,
-            "2000-01-01"
-        }
-    };
-
-
-        stockCollection =
-            MakeStockCollection(
-                testStock,
-                priceData2);
-
-
-        Result result2 =
-            stockCollection
-            .GetBiggestChange();
-
-
-
-        Debug.Assert(
-            result2.Difference
-            == -30);
-
-        Debug.Assert(
-            result2.StartDate
-            == "1999-12-30");
-
-        Debug.Assert(
-            result2.EndDate
-            == "2000-01-01");
-    }
 }
+
+
+Console.ReadLine();
+Console.WriteLine("Hello, World!");
